5645b17d681edbe550c64c43db59dd47beb0fff3
GKmargo <giovanitccamargo@gmail.com>
2024-06-26 09:49:15 -0300

    Progresso  do Projeto

---
+++ SimuladoProva/src/br/edu/up/controle/ControleDePessoas.java
@@ -0,0 +1,9 @@
+package br.edu.up.controle;
+
+import java.util.List;
+
+import br.edu.up.modelo.Pessoa;
+
+public class ControleDePessoas {
+    private List<Pessoa> pessoas;
+}
--- SimuladoProva/src/br/edu/up/modelo/Pessoa.java
+++ SimuladoProva/src/br/edu/up/modelo/Pessoa.java
@@ -1,5 +1,60 @@
 package br.edu.up.modelo;
 
 public class Pessoa {
+    private String codigo;
+    private String nome;
+    private String rua;
+    private String cidade;
+    
+    public Pessoa() {
+    }
+    
+    public Pessoa(String codigo, String nome, String rua, String cidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.rua = rua;
+        this.cidade = cidade;
+    }
 
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [codigo=" + codigo + ", nome=" + nome + ", rua=" + rua + ", cidade=" + cidade + "]";
+    }
+
+    public String toPessoaCSV(){
+        return codigo + ";" + nome; 
+    }
+
+    public String toEnderecoCSV(){
+        return rua + ";" + cidade + ";" + codigo;
+    }
+
+    public String toEnderecoPessoaCSV(){
+        return codigo + ";" + nome + ";" + rua + ";" + cidade;
+    }
 }
